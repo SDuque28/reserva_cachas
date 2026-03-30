7a51a021b2496bcf7d47140c8df8ba524577b688
SDuque28 <santiago.duquer@autonoma.edu.co>
2026-03-30 16:08:15 -0500

    feat: init Spring Boot API and React Native mobile app

---
+++ resrva-canchas-SB/src/test/java/com/backend/resrva_canchas/ResrvaCanchasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.resrva_canchas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResrvaCanchasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
